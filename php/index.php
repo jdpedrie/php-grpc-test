1a431bde7d3ec3a1cae5a69459ab8093900ca466
Daniel Sims <danielgarthsims@gmail.com>
2018-12-31 10:33:48 -0500

    Adds Slim to PHP

--- php/index.php
+++ php/index.php
@@ -1,3 +1,14 @@
 <?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-echo 'PID: ' . getmypid();
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/echo', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Hello!");
+
+    return $response;
+});
+
+$app->run();
