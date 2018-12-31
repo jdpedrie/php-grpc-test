87d3050e7bd314d88a30d3d40fb95598f92e3188
Daniel Sims <danielgarthsims@gmail.com>
2018-12-31 10:43:15 -0500

    Wiring up grpc client in PHP

--- php/index.php
+++ php/index.php
@@ -5,8 +5,20 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require 'vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/echo', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Hello!");
+
+$client = new \Example\ServiceClient("grpc_server:8080", [
+    'credentials' => \Grpc\ChannelCredentials::createInsecure()
+]);
+
+$app->get('/echo', function (Request $request, Response $response, array $args) use ($client) {
+    $echoRequest = new \Example\EchoRequest();
+    $echoRequest->setMessage(uniqid());
+
+    $call = $client->Echo($echoRequest);
+    /** @var \Example\EchoResponse $echoResponse */
+    $echoResponse = $call->wait()[0];
+
+    $response->getBody()->write($echoResponse->getMessage() . " took " . $echoResponse->getTime() . " ms");
 
     return $response;
 });
